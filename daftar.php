ba2f75a737c7aa048307a22bc484439560d2c312
windy <71083075+windyoc28@users.noreply.github.com>
2020-10-23 08:32:14 +0700

    UPDATE

---
+++ daftar.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>babycaca.id</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-2">
+           <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Babycaca</a></h1>
+          </div>
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="index.html" class="nav-link">Home</a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+
+    
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/logo.jpg);" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-5 mx-auto mt-lg-5 text-center">
+            <h1 class="text-shadow">SEPATU IMPORT</h1>
+              <p class="mb-5 text-shadow">Tampil trendi dengan sepatu kekinian yang membuat semua mata tertuju kepada anda</p>
+            
+          </div>
+        </div>
+      </div>
+
+      <a href="#property-details" class="smoothscroll arrow-down"><span class="icon-arrow_downward"></span></a>
+    </div>  
+
+    
+    
+    <div class="site-section" id="property-details">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            
+          </div>
+          <div class="container">
+    <div class="row" id="DAFTAR">
+      <div class="col s12">
+        <h3 class="center grey-text light">REGISTRASI AKUN</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s3"></div>
+      <div class="col s6">
+        <div>
+          <form method="POST" action="">
+            
+            <div class="row">
+              <div class="col s12">
+                <div class="input-field">
+                  <input name="nama_user" placeholder="Masukkan Nama" id="first_name" type="text" class="validate" required="" oninvalid="this.setCustomValidity('ISI DATA NAMA TERLEBIH DAHULU')" oninput="setCustomValidity('')" autocomplete="off">
+                  <label for="first_name">Nama</label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <div class="input-field">
+                  <input name="nama_anak" placeholder="Masukkan Nama Anak" id="first_name" type="text" class="validate" required="" oninvalid="this.setCustomValidity('ISI DATA NAMA ANAK TERLEBIH DAHULU')" oninput="setCustomValidity('')" autocomplete="off">
+                  <label for="first_name">Nama Anak</label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <div class="input-field">
+                  <input name="umur_anak_user" placeholder="Masukkan Umur Anak" id="first_name" type="text" class="validate" required="" oninvalid="this.setCustomValidity('ISI DATA UMUR ANAK TERLEBIH DAHULU')" oninput="setCustomValidity('')" autocomplete="off">
+                  <label for="first_name">Umur Anak</label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <div class="input-field">
+                  <input name="username_user" placeholder="Masukkan Username" id="first_name" type="text" class="validate" required="" oninvalid="this.setCustomValidity('ISI DATA USERNAME TERLEBIH DAHULU')" oninput="setCustomValidity('')" autocomplete="off">
+                  <label for="first_name">Username</label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <div class="input-field">
+                  <input name="password_user" placeholder="Masukkan Password" id="first_name" type="text" class="validate" required="" oninvalid="this.setCustomValidity('ISI DATA PASSWORD TERLEBIH DAHULU')" oninput="setCustomValidity('')" autocomplete="off">
+                  <label for="first_name">Password</label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <a href="index.php" class="btn blue darken-4">KEMBALI</a>
+                <button type="submit" name="simpan" class="btn right blue darken-4">REGISTRASI</button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col s12 center">
+                Sudah punya akun? <a href="login.php"><b>Login disini</b></a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="col s3"></div>
+    </div>
+  </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="row">
+              
+              
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+  </div> <!-- .site-wrap -->
+
+  <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
