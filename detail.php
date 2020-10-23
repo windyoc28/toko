ee0fabd40e8704c1c43e67e22a36bd969e300469
windy <71083075+windyoc28@users.noreply.github.com>
2020-10-23 08:07:09 +0700

    detail.php

---
+++ detail.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>babycaca.id</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-2">
+           <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Babycaca</a></h1>
+          </div>
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="index.html#home-section" class="nav-link">Home</a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+
+    
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/logo.jpg);" data-aos="fade">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-5 mx-auto mt-lg-5 text-center">
+            <h1 class="text-shadow">SEPATU IMPORT</h1>
+              <p class="mb-5 text-shadow">Tampil trendi dengan sepatu kekinian yang membuat semua mata tertuju kepada anda</p>
+            
+          </div>
+        </div>
+      </div>
+
+      <a href="#property-details" class="smoothscroll arrow-down"><span class="icon-arrow_downward"></span></a>
+    </div>  
+
+    
+    
+    <div class="site-section" id="property-details">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="owl-carousel slide-one-item with-dots">
+              <div><img src="images/sepatu.jpg" alt="Image" class="img-fluid"></div>
+              <div><img src="images/sepatu1.jpeg" alt="Image" class="img-fluid"></div>
+              <div><img src="images/sepatu2.jpeg" alt="Image" class="img-fluid"></div>
+              <div><img src="images/sepatu3.jpeg" alt="Image" class="img-fluid"></div>
+              <div><img src="images/sepatu5.jpg" alt="Image" class="img-fluid"></div>
+              <div><img src="images/sepatu7.jpeg" alt="Image" class="img-fluid"></div>
+            </div>
+          </div>
+          <div class="col-lg-5 pl-lg-5 ml-auto">
+            <div class="mb-5">
+              <h3 class="text-black mb-4">Detail Barang</h3>
+              <p>Bahan PU berkualitas</p>
+              <p>ukuran 35 = 22-22,5cm</p>
+              <p>ukuran 36 = 22,5-23cm</p>
+              <p>ukuran 37 = 23-23,5cm</p>
+              <p>ukuran 38 = 23,5-24cm</p>
+              <p>ukuran 39 = 24,5cm</p>
+              <p>ukuran 40 = 24,5-25cm</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="row">
+              
+              
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+  </div> <!-- .site-wrap -->
+
+  <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>babycaca.id</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+<!-- header -->
+
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-3">
+            <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">Babycaca Shop</a></h1>
+          </div>
+
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="?=page/marketplace/marketplace/home/index.php" class="nav-link">HOME</a></li>
+                <li><a href="index.html#home-section" class="nav-link">Keranjang</a></li>
+                <li><a href="index.html#home-section" class="nav-link">Masuk</a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+
+    
+    <div class="site-block-wrap">
+    <div class="owl-carousel with-dots">
+      <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/logo.jpg);" data-aos="fade" id="home-section">
+
+
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 mt-lg-5 text-center">
+              <h1 class="text-shadow">SEPATU IMPORT</h1>
+              <p class="mb-5 text-shadow">Tampil trendi dengan sepatu kekinian yang membuat semua mata tertuju kepada anda</p>
+              <p><a href="#" target="_blank" class="btn btn-primary px-5 py-3">Get Started</a></p>
+              
+            </div>
+          </div>
+        </div>
+  
+        
+      </div>  
+  
+       
+    </div>   
+    
+  </div>      
+
+
+  <div class="site-section" id="properties-section">
+      <div class="container">
+        <div class="row large-gutters">
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+            <div class="ftco-media-1">
+              <div class="ftco-media-1-inner">
+                <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu.jpg" alt="FImageo" class="img-fluid"></a>
+                <div class="ftco-media-details">
+                  <h3>SEPATU WANITA</h3>
+                  <strong>RP. 200.000</strong>
+                </div>
+  
+              </div> 
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                  <div class="ftco-media-1-inner">
+                    <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu1.jpeg" alt="Image" class="img-fluid"></a>
+                    <div class="ftco-media-details">
+                      <h3>SEPATU WANITA</h3>
+                      <strong>RP. 210.000</strong>
+                    </div>
+      
+                  </div> 
+                </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                  <div class="ftco-media-1-inner">
+                    <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu2.jpeg" alt="Image" class="img-fluid"></a>
+                    <div class="ftco-media-details">
+                      <h3>SEPATU WANITA</h3>
+                      <strong>RP.200.000</strong>
+                    </div>
+      
+                  </div> 
+                </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu3.jpeg" alt="Image" class="img-fluid"></a>
+                  <div class="ftco-media-details">
+                    <h3>SEPATU WANITA</h3>
+                    <strong>RP.220.000</strong>
+                  </div>
+    
+                </div> 
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                <div class="ftco-media-1">
+                    <div class="ftco-media-1-inner">
+                      <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu7.jpeg" alt="Image" class="img-fluid"></a>
+                      <div class="ftco-media-details">
+                        <h3>SEPATU WANITA</h3>
+                        <strong>RP.210.000</strong>
+                      </div>
+        
+                    </div> 
+                  </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                <div class="ftco-media-1">
+                    <div class="ftco-media-1-inner">
+                      <a href="detail.html" class="d-inline-block mb-4"><img src="images/sepatu5.jpg" alt="Image" class="img-fluid"></a>
+                      <div class="ftco-media-details">
+                        <h3>SEPATU WANITA</h3>
+                        <strong>RP.200.000</strong>
+                      </div>
+        
+                    </div> 
+                  </div>
+            </div>
+
+        </div>
+      </div>
+    </div>
+    <section class="site-section bg-light bg-image" id="contact-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <h2 class="section-title mb-3">Hubungi kami segera</h2>
+          </div>
+        </div>
+          <div class="col-md-5">
+            
+            <div class="p-4 mb-3 bg-white">
+              <p class="mb-0 font-weight-bold">Alamat</p>
+              <p class="mb-4">Jl. Pulau Seribu no.29, Kauman, Ketapang</p>
+
+              <p class="mb-0 font-weight-bold">Whatsapp</p>
+              <p class="mb-4"><a href="#">0895631176817</a></p>
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="row">
+              
+              
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+  </div> <!-- .site-wrap -->
+
+  <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
